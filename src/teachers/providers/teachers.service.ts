9a75dbe7ab0efa67db83553bea5f7964bd0ba2d8
MiraProfissional <jvitor.gmira@gmail.com>
2024-11-11 09:51:40 -0300

    :sparkles: feat: creating GET route for Teachers

--- src/teachers/providers/teachers.service.ts
+++ src/teachers/providers/teachers.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { GetUsersParamDto } from '../dtos/get-users-param.dto';
 
 @Injectable()
-export class TeachersService {}
+export class TeachersService {
+  public async getTeachers(getUsersParamDto: GetUsersParamDto) {
+    if (getUsersParamDto.id) {
+      return this.findOneById(getUsersParamDto.id);
+    }
+    return this.findAll();
+  }
+
+  public async findAll() {
+    return true;
+  }
+
+  public async findOneById(id: number) {
+    return id;
+  }
+}
--- src/teachers/teachers.controller.ts
+++ src/teachers/teachers.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { TeachersService } from './providers/teachers.service';
 
 @Controller('teachers')
-export class TeachersController {}
+export class TeachersController {
+  constructor(private readonly teachersService: TeachersService) {}
+
+  @ApiOperation({
+    summary: 'Fetches a list of registered users on the application',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Users fetched successfully based on the query',
+  })
+  @Get('/:id?')
+  public getTeachers(@Param() getUsersParamDto: GetUsersParamDto) {
+    return this.teachersService.getTeachers(getUsersParamDto);
+  }
+}
