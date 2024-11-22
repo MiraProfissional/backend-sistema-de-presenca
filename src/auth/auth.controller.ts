bdd8c93791a5638f8f9e53066a50a381ee84b3e7
MiraProfissional <jvitor.gmira@gmail.com>
2024-11-22 14:13:26 -0300

    :sparkles: feat: adding swagger decorators for Auth routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,11 +4,20 @@ import { SignInDto } from './dtos/signin.dto';
 import { Auth } from './decorators/auth.decorator';
 import { AuthType } from './enums/auth-type.enum';
 import { RefreshTokenDto } from './dtos/refresh-token.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('Auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({
+    summary: 'Checks user authentication on the application',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User authenticated successfully',
+  })
   @Post('sign-in')
   @HttpCode(HttpStatus.OK)
   @Auth(AuthType.None)
@@ -16,6 +25,13 @@ export class AuthController {
     return this.authService.signIn(signInDto);
   }
 
+  @ApiOperation({
+    summary: 'Returns a valid token based on a valid refresh token',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Refresh token returned successfully',
+  })
   @Post('refresh-tokens')
   @HttpCode(HttpStatus.OK)
   @Auth(AuthType.None)
