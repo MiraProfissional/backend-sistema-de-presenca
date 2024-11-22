7f13a694e645a7212123e44652741d1f8b1b45b8
MiraProfissional <jvitor.gmira@gmail.com>
2024-11-22 11:22:06 -0300

    :sparkles: feat: adding description to create-discipline dto

--- src/disciplines/dtos/create-discipline.dto.ts
+++ src/disciplines/dtos/create-discipline.dto.ts
@@ -1,18 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateDisciplineDto {
+  @ApiProperty({
+    description: 'This is the name of the discipline',
+    example: 'Gerência de Projetos de Software',
+  })
   @IsString()
   @MinLength(3)
   @MaxLength(48)
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'This is the code of the discipline',
+    example: 'SDES06',
+  })
   @IsString()
   @MinLength(3)
   @MaxLength(12)
   @IsNotEmpty()
   code: string;
 
+  @ApiProperty({
+    description:
+      "This is the camera's ip responsible to capture the discipline's students",
+    example: '192.168.1.101',
+  })
   @IsString()
   @IsNotEmpty()
   ipCamera: string;
